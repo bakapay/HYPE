335b2c4880bad0d855e25886f0a002ec2a35ce4b
zapayyyy <36130036+zapayyyy@users.noreply.github.com>
2018-02-04 18:35:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -131,14 +131,5 @@ const embed = new Discord.RichEmbed()
 });
 
 
-client.on("message", message => {
-	if(message.content.startsWith("/say")){	
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("nu ai **acces** la aceasta comanda.")
-let mesaj = message.content.split(" ").slice(0);
-message.channel.send(mesaj)
-}
-});
-
-
 
 client.login("NDA5MzcyODcwMjU5NjM4Mjcz.DVdpxw.IDql-6-5uGkCM8Z9gtFVGUcLnmk");
