da21bf39ca27b7743ae2c4ea261a0e113a39d405
zapayyyy <36130036+zapayyyy@users.noreply.github.com>
2018-02-04 18:00:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,4 +19,118 @@ client.on("guildMemberAdd", member => {
   member.addRole(default2role)
 });
 
+client.on("message", message => {
+if(message.content.startsWith("/acceptat"){
+  if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("nu ai acces.")
+
+  let member = message.mentions.members.first();
+  if (!member) return message.reply("trebuie să menționezi persoana pe care vrei să o accepți.")
+  let thelper = message.guild.roles.find("name", "Hype - Testers")
+  let partner = message.guild.roles.find("name", "Server Manager")
+  
+  if(message.channel.id === "409294069282504705"){
+   client.channels.get("409294069282504705").send(`Felicitări, ${member}. Cererea ta de parteneriat a fost acceptată de către ${message.author}.`)
+   member.addRole(partner.id)}
+   
+  if(message.channel.id === "409294041734316032"){
+   client.channels.get("409294041734316032").send(`Felicitări, ${member}. Cererea ta de grad a fost acceptată de către ${message.author}.`)
+  member.addRole(thelper.id)}
+ 
+  message.delete()
+}
+});
+
+client.on("message", message => {
+	if(message.content.startsWith("/color"){
+let arg = message.content.split("").slice(0)
+let sender = message.author
+let yellow = message.guild.roles.find("name", "yellow");
+let red = message.guild.roles.find("name", "red");
+let blue = message.guild.roles.find("name", "blue");
+let purple = message.guild.roles.find("name", "purple");
+let green = message.guild.roles.find("name", "green");
+let golden = message.guild.roles.find("name", "golden");
+let black = message.guild.roles.find("name", "black");
+let white = message.guild.roles.find("name", "white");
+let gray = message.guild.roles.find("name", "gray");
+let pink = message.guild.roles.find("name", "pink");
+
+if(!arg || arg === "list") return message.author.send("**==== CULORI DISPONIBILE ====**\n**/color yellow** - galben\n**/color red** - rosu\n**/color blue** - albastru\n**/color purple** - violet\n**/color green** - verde\n**/color golden** - auriu\n**/color black** - negru\n**/color white** - alb\n**/color gray** gri\n**/color pink** - roz")
+	
+if(arg === "yellow") return message.guild.member(sender).addRole(yellow)
+if(arg === "red") return message.guild.member(sender).addRole(red)
+if(arg === "blue") return message.guild.member(sender).addRole(blue)
+if(arg === "purple") return message.guild.member(sender).addRole(purple)
+if(arg === "green") return message.guild.member(sender).addRole(green)
+if(arg === "golden") return message.guild.member(sender).addRole(golden)
+if(arg === "black") return message.guild.member(sender).addRole(black)
+if(arg === "white") return message.guild.member(sender).addRole(white)
+if(arg === "gray") return message.guild.member(sender).addRole(gray)
+if(arg === "pink") return message.guild.member(sender).addRole(pink)
+	message.delete()
+}
+});
+
+client.on("message", message => {
+	if(message.content.startsWith("/leave"){
+        let kickMember = message.author
+	kickMember.send("Ai iesit de pe serverul **HYPEUNIVERSE**, daca vrei sa revii pe server, poti folosi link-ul de mai jos. \n:link: https://discord.gg/t6HGuCe")
+	message.guild.member(kickMember).kick()
+}	
+});
+
+client.on("message", message => {
+	if(message.content.startsWith("/report"){
+	   let member = message.mentions.members.first();
+let motiv = message.content.split("").slice(1)
+let dovada = message.content.split("").slice(2)
+
+if(!member) return message.reply("trebuie sa **mentionezi** persoana reclamata.")
+if(!motiv) return message.reply("trebuie sa **prezinti** un motiv, obligatoriu sa fie compus **dintr-un cuvant**.")
+if(!dovada) return message.reply("trebuie sa prezinti si o **dovada**, obligatoriu sa fie un **link**.")
+	
+let report = new Discord.RichEmbed()
+.setAuthor(member.user.username, member.user.avatarURL)
+.setTitle("RECLAMATIE | HYPEUNIVERSE")
+.addField("Utilizator", member.user.username, true)
+.addField("Motiv", motiv, true)
+.addField("Dovada", "[CLICK](" + dovada + ")", true)
+.setFooter("Reclamat de catre " + message.author.username)
+.setTimestamp()
+.setColor(0x0896c7)
+
+client.channels.get("409294199415111681").sendEmbed(report)
+message.delete();
+	   }
+	   });
+
+client.on("message", message => {
+	if(message.content.startsWith("/serverinfo"){
+const embed = new Discord.RichEmbed()
+	.setAuthor(message.guild, message.guild.iconURL)
+	.addField("Name", message.guild, true)
+	.addField("Owner", message.guild.owner, true)
+	.addField("Members", message.guild.memberCount, true)
+	.addField("Online", message.guild.members.filter(m => m.presence.status !== 'offline').size, true)
+	.addField("Channels", message.guild.channels.size, true)
+	.addField("Region", message.guild.region, true)
+	.setFooter("CreatedAt • Friday, February 02, 2018 at 6:20 PM")
+	.setThumbnail(message.guild.iconURL)
+        .setColor(0x0896c7)
+	    message.channel.sendEmbed(embed)
+}
+});
+
+
+client.on("message", message => {
+	if(message.content.startsWith("/say"){	
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("nu ai **acces** la aceasta comanda.")
+let mesaj = message.content.split("").slice(0)
+message.channel.send(mesaj)
+message.delete()
+}
+});
+
+
+
 client.login("NDA5MzcyODcwMjU5NjM4Mjcz.DVdpxw.IDql-6-5uGkCM8Z9gtFVGUcLnmk");
